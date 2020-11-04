dd81585ca38c326386b1c2c74e9769bf10e8d100
Leiya Kenney <leiya.kenney@gmail.com>
2020-11-04 14:22:41 -0700

    Update this.state to be monsters array instead of generic phrase

--- src/App.js
+++ src/App.js
@@ -5,21 +5,32 @@ import './App.css';
 class App extends Component {
   constructor () {
     super();
+
     this.state = {
-      string: 'Hello Leiya'
+      monsters: [
+        {
+          name: 'Frankenstein',
+          id: 'asc1'
+        },
+        {
+          name: 'Dracula',
+          id: 'asc2'
+        },
+        {
+          name: 'Zombie',
+          id: 'asc3'
+        }
+      ]
     };
   }
 
   render () {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>{this.state.string}</p>
-          <button onClick={() => this.setState({ string: 'Sup Leiya'})}> 
-            Change Text
-          </button>
-        </header>
+        {
+          this.state.monsters.map(monster => 
+            <h1 key={monster.id}> {monster.name} </h1>)
+        }
       </div>
     );
   }
