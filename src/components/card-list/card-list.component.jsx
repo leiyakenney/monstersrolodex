320fcc059417172db9bed5c02f1c6f0cb3bdc5d5
Leiya Kenney <leiya.kenney@gmail.com>
2020-11-04 16:59:04 -0700

    Update card components

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,8 +1,8 @@
  import React from 'react';
 
- import './card-list.styles.css'
-
  import { Card } from '../card/card.component';
+
+ import './card-list.styles.css'
 
  export const CardList = props => (
     <div className='card-list'>
